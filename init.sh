78e5f2b848c9271041d4df0d52c1d3e85905b241
Guilhem <guilhem99.rolland@gmail.com>
2022-04-20 11:51:19 +0000

    automatized clone

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# A-Eye Visor
+git clone https://github.com/GuilhemROLLAND/A-Eye-Visor.git
+# Communication Module
+git clone https://github.com/GuilhemROLLAND/CommunicationModule.git
+# Image Module
+git clone https://github.com/GuilhemROLLAND/ImageModule.git
